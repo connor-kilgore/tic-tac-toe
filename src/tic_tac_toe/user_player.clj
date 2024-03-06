1344b46bc5c9b0ee5163ae569aff5574b0eb1e36
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-03-06 09:31:17 -0700

    update user-player

--- src/tic_tac_toe/user_player.clj
+++ src/tic_tac_toe/user_player.clj
@@ -2,12 +2,12 @@
   (:require [tic-tac-toe.menu-selector :as menu]
             [tic-tac-toe.tic-tac-toe-board :as tttb]))
 
-(defn parse-position [row col]
-  (+ (* row 3) col))
+(defn parse-position [side-len row col]
+  (+ (* row side-len) col))
 
 (defn play-turn [board symbol]
-  (let [position (parse-position (menu/get-selection menu/rows-options) (menu/get-selection menu/cols-options))
+  (let [position (parse-position (int (Math/sqrt (count board))) (menu/get-selection (get menu/rows-options (count board))) (menu/get-selection (get menu/cols-options (count board))))
         new-board (tttb/place-value-into-tttb board symbol position)]
     (if (not (= new-board board))
       new-board
-      (do (println "\nSpace already taken, try again.") (recur symbol board)))))
+      (do (println "\nSpace already taken, try again.") (recur board symbol)))))
