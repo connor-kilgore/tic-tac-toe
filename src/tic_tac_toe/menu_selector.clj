3cfcea48f6c27ebcb1fc5b657161296d370f816a
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-03-06 09:23:23 -0700

    Updated menu-selector

--- src/tic_tac_toe/menu_selector.clj
+++ src/tic_tac_toe/menu_selector.clj
@@ -1,36 +1,48 @@
 (ns tic-tac-toe.menu-selector)
 
 (defn print-start-screen []
-  (println "Welcome to the Unsolvable Tic Tac Toe!"))
+  (println "\nWelcome to the Unsolvable Tic Tac Toe!"))
 
 (defn close-program []
   (println "\nClosing Program! Goodbye."))
 
 (def retry-options
   {:print-statement (fn [] (println "\nWould you like to try again?\n[1] Yes\n[2] No"))
-   :error (fn [] (println "\nPlease select a valid option."))
+   :error           (fn [] (println "\nPlease select a valid option."))
    "1"              true
    "2"              false})
 
 (def symbol-options
   {:print-statement (fn [] (println "\nPlease select a symbol!\n[X] (first turn).\n[O] (second turn)."))
-   :error (fn [] (println "\nPlease select a valid symbol."))
+   :error           (fn [] (println "\nPlease select a valid symbol."))
    "x"              1 "X" 1
    "o"              2 "O" 2})
 
 (def rows-options
-  {:print-statement (fn [] (println "\nSelect a row [0-2]"))
-   :error (fn [] (println "\nPlease select a valid row."))
-   "0"              0
-   "1"              1
-   "2"              2})
+  {9 {:print-statement (fn [] (println "\nSelect a row [0-2]"))
+      :error           (fn [] (println "\nPlease select a valid row."))
+      "0"              0
+      "1"              1
+      "2"              2}
+   16 {:print-statement (fn [] (println "\nSelect a row [0-3]"))
+       :error           (fn [] (println "\nPlease select a valid row."))
+       "0"              0
+       "1"              1
+       "2"              2
+       "3"              3}})
 
 (def cols-options
-  {:print-statement (fn [] (println "Select a column [0-2]"))
-   :error (fn [] (println "\nPlease select a valid column."))
-   "0"              0
-   "1"              1
-   "2"              2})
+  {9 {:print-statement (fn [] (println "Select a column [0-2]"))
+      :error           (fn [] (println "\nPlease select a valid column."))
+      "0"              0
+      "1"              1
+      "2"              2}
+   16 {:print-statement (fn [] (println "Select a column [0-3]"))
+       :error           (fn [] (println "\nPlease select a valid column."))
+       "0"              0
+       "1"              1
+       "2"              2
+       "3"              3}})
 
 (defn get-option [selected-map option]
   (get selected-map option))
